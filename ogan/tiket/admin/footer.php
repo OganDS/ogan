387667fded234a39682e0f2d14416d9f2727d267
Chitogechan <73518108+Chitogechan@users.noreply.github.com>
2022-03-26 03:16:43 +0700

    Add files via upload

---
+++ ogan/tiket/admin/footer.php
@@ -0,0 +1,21 @@
+<footer class="page-footer" style="background-color: black;">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">BayPlane</h5>
+                <p class="grey-text text-lightent-4">Pesan tiket hanya dengan 1 klik.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+               <h5 class="white-text">Silahkan Ikuti Kami di</h5>
+                <ul>
+                    <li><a href="https://Facebook.com/ogands/" class="grey-text text-lighten-4" target="_blank">
+                        <i class="ion-social-facebook"></i>  Facebook</a> <br/></li>
+                    <li><a href="https://Instagram.com/ogan.ds/" class="grey-text text-lighten-4" target="_blank">
+                        <i class="ion-social-instagram"></i>  Instagram</a> <br/></li>                
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</footer>
